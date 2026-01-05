0eb7e09c0352858b663a8ce4bfd382d1ecba3d67
shiroko <15045386798@163.com>
2026-01-05 12:49:27 +0800

    Add files via upload

---
+++ comments/waline.js
@@ -0,0 +1,47 @@
+import { init, commentCount  } from 'https://unpkg.com/@waline/client@v3/dist/waline.js';
+import { pageviewCount } from 'https://unpkg.com/@waline/client@v3/dist/pageview.js';
+
+(() => {
+  const serverURL = 'https://shirokocomment-czvrlk8ci-fallwind2233s-projects.vercel.app/';
+  let firstVisit = true;
+
+  const loadComments = async () => {
+    const container = document.getElementById('w-comments');
+    if (!!container) {
+      // 是文章或页面，完整加载 Waline
+      const path = container.getAttribute("data-path");
+      init({
+        el: container,
+        path,
+        dark: 'html[data-theme="dark"]',
+        serverURL,
+        pageview: true,
+        comment: true,
+      });
+    } else {
+      // 是首页，只展示页面访问和评论数量，不渲染评论区
+      pageviewCount({
+        serverURL,
+        update: false,
+      });
+      commentCount({
+        serverURL,
+      });
+    }
+
+    if (firstVisit) {
+      // 站点的访问统计，仅生效一次
+      firstVisit = false;
+      pageviewCount({
+        serverURL,
+        path: "/index.html",
+      });
+    }
+  };
+
+  window.loadComments = loadComments;
+  window.addEventListener('pjax:success', () => {
+    window.loadComments = loadComments;
+  });
+
+})();
